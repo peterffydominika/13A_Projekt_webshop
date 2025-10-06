4ccbf1027a0f1fa6185d9d6fe41736061c156c4b
peterffydominika <90351361+peterffydominika@users.noreply.github.com>
2025-10-06 21:06:01 +0200

    Add files via upload

--- ProjektAdmin/WpfApp1/WpfApp1/Termekek.xaml.cs
+++ ProjektAdmin/WpfApp1/WpfApp1/Termekek.xaml.cs
@@ -1,24 +1,59 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp1
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private ObservableCollection<Termek> termekek;  // Globális változó a gyűjteményhez
+        private Random random = new Random();  // Random a generáláshoz
+
         public MainWindow()
         {
             InitializeComponent();
+
+            // Inicializáld a gyűjteményt
+            termekek = new ObservableCollection<Termek>();
+
+            // Generáljunk 10 random terméket induláskor (mint korábban)
+            for (int i = 1; i <= 10; i++)
+            {
+                Termek ujTermek = new Termek
+                {
+                    Id = i,
+                    Nev = "Termék" + random.Next(1, 1001),
+                    Mennyiseg = random.Next(1, 101),
+                    Egysegar = Math.Round(random.NextDouble() * 450 + 50, 2)
+                };
+                termekek.Add(ujTermek);
+            }
+
+            // Kösd az adatokat
+            dataGrid.ItemsSource = termekek;
+        }
+
+        private void Button_UjTermekBeszurasa(object sender, RoutedEventArgs e)
+        {
+            // Új random termék létrehozása
+            Termek ujTermek = new Termek
+            {
+                Id = termekek.Count + 1,  // Következő ID
+                Nev = "Új Termék" + random.Next(1, 1001),
+                Mennyiseg = random.Next(1, 101),
+                Egysegar = Math.Round(random.NextDouble() * 450 + 50, 2)
+            };
+
+            // Beszúrás a gyűjteménybe (pl. a végére)
+            termekek.Add(ujTermek);
+
+            // Opcionális: Görgess az új sorhoz
+            dataGrid.ScrollIntoView(ujTermek);
+        }
+
+        private void DataGrid_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            // Itt kezeld a kijelölést, ha szükséges
         }
     }
 }
